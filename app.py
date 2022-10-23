20df0037532af504ec6f37a5a06660deae131c98
malik <maliknaik16@gmail.com>
2022-10-23 05:34:05 -0400

    Update the codebase with new working code including templates and images

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
 
+from flask import Flask, request, render_template, send_from_directory, url_for, redirect
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-from flask import Flask, request, render_template
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 
 import numpy as np
 import pandas as pd
 import requests
 import pickle
 import json
+import os
+
+load_dotenv('.env')
 
 app = Flask(__name__)
+API_KEY = os.environ.get('MAPS_API_KEY')
+BASE_URL = os.environ.get('BASE_URL')
 
+debug = os.environ.get('DEBUG')
+debug = True if debug == 'true' else False
 model = None
 
 options = webdriver.ChromeOptions()
@@ -22,29 +30,71 @@ options = webdriver.ChromeOptions()
 # for option in opts:
 #     options.add_argument(option)
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+service = Service(ChromeDriverManager().install())
 
 with open('model.pkl', 'rb') as m:
 
     # Safely loading the model.
     model = pickle.load(m)
 
+
+@app.route('/images/<path:path>')
+def send_image(path):
+    return send_from_directory('images', path)
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/analyze')
+def analyze():
+    return render_template('analyze.html')
+
+@app.route('/good')
+def good():
+    return render_template('good.html')
+
+@app.route('/bad', methods=['POST'])
+def bad(latlng='33.777252127593584,-84.39540740633934'):
+
+    if 'latlng' in request.args:
+        latlng = request.args['latlng']
+
+    gmaps_url = 'https://maps.googleapis.com/maps/api/geocode/json?key=%s&latlng=%s' % (API_KEY, latlng)
+
+    gmap_resp = requests.get(gmaps_url).json()
+
+    loc = gmap_resp['results'][0]['formatted_address']
+
+    l = loc.strip().split(',')
+    city = l[-3].strip()
+    x = l[-2].strip().split(' ')
+    zipcode = x[1].strip()
+    state = x[0].strip()
+
+    # doctors_list = doctors()
+    doctors_list = requests.get('%s/doctors' % (BASE_URL, ), params={'city': city, 'zipcode': zipcode, 'state': state}).json()
+
+    return render_template('bad.html', doctors=doctors_list['doctors'])
+
 @app.route('/predict', methods = ['POST'])
 def predict():
+
 
     if not model:
         return {'message': 'Sorry an internal server error occurred.'}
 
+    form_vals = list(request.form.values())
+    latlng = form_vals[-1].strip()
+
+    if len(latlng) < 3:
+        latlng = '33.777252127593584,-84.39540740633934'
+
     d = {}
     cols =['Age', 'Sex', 'ChestPainType', 'RestingBP', 'Cholesterol', 'FastingBS', 'RestingECG', 'MaxHR', 'ExerciseAngina', 'Oldpeak', 'ST_Slope']
 
-    form_vals = list(request.form.values())
 
-    for i in range(len(form_vals)):
+    for i in range(len(form_vals) - 1):
         if cols[i] not in d:
             d[cols[i]] = []
 
@@ -53,94 +103,97 @@ def predict():
     data = pd.DataFrame.from_dict(d)
     prediction = model.predict(data)
 
-    output = prediction
+    prediction = int(prediction[0])
 
-    if output == 0:
-        return render_template('index.html',
-                               result = 'The patient is not likely to have heart disease!')
-    else:
-        return render_template('index.html',
-                               result = 'The patient is likely to have heart disease!')
+    if prediction == 0:
+        return redirect(url_for('good'))
 
+
+    return redirect(url_for('bad'), code=307)
 
 @app.route('/doctors')
 def doctors():
 
     args = request.args
-
-    if 'city' not in args:
-        return {'message': 'City not found.'}
-
-    if 'zipcode' not in args:
-        return {'message': 'Zipcode not found.'}
-
-
-    if 'state' not in args:
-        return {'message': 'State not found.'}
-
-    city = args['city']
-    zipcode = args['zipcode']
-    state = args['state']
-
-    url = 'https://doctor.webmd.com/results?q=Heart%20Doctor&d=40&pagenumber=1' + '&zc=%s&city=%s&state=%s' % (zipcode, city, state)
-
-    driver.get(url)
-
-    content = driver.page_source
-
-    soup = BeautifulSoup(content, 'lxml')
-
-    cards = soup.findAll('div', {'class': 'results-card-wrap'})
-
     doctors = []
 
-    for card in cards:
+    if not debug:
 
-        doctor = {}
+        if 'city' not in args:
+            return {'message': 'City not found.'}
 
-        anchor = card.find('a', {'class': 'prov-name'})
+        if 'zipcode' not in args:
+            return {'message': 'Zipcode not found.'}
 
-        link = anchor['href'] if anchor else ''
 
-        image = ''
+        if 'state' not in args:
+            return {'message': 'State not found.'}
 
-        img = card.find('div', {'class': 'card-img with-url'})
-        img = img.find('img') if img else None
-        image = img['src'] if img else ''
+        city = args['city']
+        zipcode = args['zipcode']
+        state = args['state']
 
-        doctor['url'] = link
-        doctor['img_url'] = image
+        url = 'https://doctor.webmd.com/results?q=Heart%20Doctor&d=40&pagenumber=1' + '&zc=%s&city=%s&state=%s' % (zipcode, city, state)
 
-        name = ''
+        driver = webdriver.Chrome(service=service, options=options)
+        driver.get(url)
 
-        speciality = card.find('p', {'class': 'prov-specialty'}).get_text().strip()
-        speciality = speciality.split(',')
-        speciality = list(map(lambda x: x.strip(), speciality))
-        doctor['speciality'] = speciality
+        content = driver.page_source
 
-        if anchor:
-            name = anchor.get_text().strip()
+        soup = BeautifulSoup(content, 'lxml')
 
-        doctor['name'] = name
-        desc = card.find('div', {'class': 'prov-bio'})
-        desc = desc.get_text().strip()
+        cards = soup.findAll('div', {'class': 'results-card-wrap'})
 
-        doctor['description'] = desc
 
-        phone = card.find('button', {'data-metrics-link': 'ph'})
-        phone = phone.get_text().strip() if phone else ''
+        for card in cards:
 
-        doctor['phone'] = phone
+            doctor = {}
 
-        address = card.find('span', {'class': 'addr-text'})
-        address = address.get_text().strip() if address else ''
-        doctor['address'] = address
+            anchor = card.find('a', {'class': 'prov-name'})
 
-        rating = card.find('span', {'class': 'webmd-rate--number'})
-        rating = rating.get_text().strip() if rating else ''
-        doctor['ratings'] = rating
+            link = anchor['href'] if anchor else ''
 
-        doctors.append(doctor)
+            image = ''
+
+            img = card.find('div', {'class': 'card-img with-url'})
+            img = img.find('img') if img else None
+            image = img['src'] if img else ''
+
+            doctor['url'] = link
+            doctor['img_url'] = image
+
+            name = ''
+
+            speciality = card.find('p', {'class': 'prov-specialty'}).get_text().strip()
+            speciality = speciality.split(',')
+            speciality = list(map(lambda x: x.strip(), speciality))
+            doctor['speciality'] = speciality
+
+            if anchor:
+                name = anchor.get_text().strip()
+
+            doctor['name'] = name
+            desc = card.find('div', {'class': 'prov-bio'})
+            desc = desc.get_text().strip()
+
+            doctor['description'] = desc
+
+            phone = card.find('button', {'data-metrics-link': 'ph'})
+            phone = phone.get_text().strip() if phone else ''
+
+            doctor['phone'] = phone
+
+            address = card.find('span', {'class': 'addr-text'})
+            address = address.get_text().strip() if address else ''
+            doctor['address'] = address
+
+            rating = card.find('span', {'class': 'webmd-rate--number'})
+            rating = rating.get_text().strip() if rating else ''
+            doctor['ratings'] = rating
+
+            doctors.append(doctor)
+
+        driver.close()
 
     if len(doctors) == 0:
 
